c5edf92c3be323dcb58db9c7605378b6480e23a6
Charles Evans Ogego Otieno <ceo.ogego@gmail.com>
2018-09-22 13:23:03 +0300

    Updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>HR Management System</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -79,16 +79,16 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    HR Management System
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">Home</a>
+                    <a href="#">Payroll</a>
+                    <a href="#">Performance</a>
+                    <a href="#">Training</a>
+                    <a href="#">Leave Management</a>
+                    <a href="#">Resignation</a>
                 </div>
             </div>
         </div>
